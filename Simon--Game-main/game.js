2936ffa6dd99006a235a9ff067c34ae842dd3723
Ragini Agrawal <132738748+ragini-1230@users.noreply.github.com>
2024-05-29 23:25:58 +0530

    Add files via upload

---
+++ Simon--Game-main/game.js
@@ -0,0 +1,71 @@
+var gamePattern=[];
+var buttonColours=["red", "blue", "green", "yellow"];
+var userClickedPattern=[];
+var started=false;
+level=0;
+function nextSequence()
+{
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    level++;
+    $("#level-title").text("Level " + level);
+    userClickedPattern=[];
+}
+function playSound(name)
+{
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+$(".btn").click(function(){
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+    animatePress(userChosenColour);
+});
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
+$(".pbt").click(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+function checkAnswer(currentLevel)
+{
+  if(gamePattern[currentLevel]===userClickedPattern[currentLevel])
+  {
+    if(gamePattern.length===userClickedPattern.length)
+    {
+      setTimeout(function () {
+        nextSequence();
+      },1000);
+    }
+  }
+  else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press on Play Button to Restart");
+   
+    setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 200);
+  startOver();
+  }
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+}
